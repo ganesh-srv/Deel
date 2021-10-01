9ab755cf3fbe0a7d131096749d6987413ab0acd9
ganeshsrv-kaboom <46423582+ganeshsrv-kaboom@users.noreply.github.com>
2021-10-01 22:44:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let  csv = require("csvtojson");
+async  function acceptanceRateCalculator(){
+
+let  jsonArray = await csv().fromFile('C:/Users/gsarakad/Projects/polaris/personal/Deel/Docs/Globepay-Acceptance-Report.csv');
+// console.log("json array", jsonArray);
+let acceptanceRecords  = jsonArray.filter((record) => record.state==='ACCEPTED');
+
+console.log("Total records count", jsonArray.length);
+console.log("Acceptance records count", acceptanceRecords.length);
+
+let acceptanceRate = (acceptanceRecords.length/jsonArray.length)*100;
+
+console.log(`Acceptance Rate is ${acceptanceRate} %`);
+
+}
+
+acceptanceRateCalculator();
+
+
+
